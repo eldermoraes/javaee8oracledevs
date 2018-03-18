be73455efc9452b44195bbdf74743b9a822ce941
Elder Moraes <elder.moraes@gmail.com>
2018-03-18 15:34:49 -0700

    Fixes

---
+++ 16928_cicd/cicd/run-jenkins.sh
@@ -0,0 +1,8 @@
+docker rm -f jenkins || true
+
+docker run -d --name jenkins \
+    -p 8888:8080 -p 50000:50000 \
+    -v ${HOME}/jenkins_home:/var/jenkins_home \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(which docker):/usr/bin/docker \
+    jenkins-docker
