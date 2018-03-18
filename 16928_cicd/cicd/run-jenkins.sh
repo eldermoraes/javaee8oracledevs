04b646c467c6c1d31011eeddcca0aa97eecb3b61
Elder Moraes <elder.moraes@gmail.com>
2018-03-18 15:54:52 -0700

    CI/CD example

--- 16928_cicd/cicd/run-jenkins.sh
+++ 16928_cicd/cicd/run-jenkins.sh
@@ -5,4 +5,5 @@ docker run -d --name jenkins \
     -v ${HOME}/jenkins_home:/var/jenkins_home \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v $(which docker):/usr/bin/docker \
+    -v ${HOME}/.m2:/var/jenkins_home/.m2 \
     jenkins-docker
