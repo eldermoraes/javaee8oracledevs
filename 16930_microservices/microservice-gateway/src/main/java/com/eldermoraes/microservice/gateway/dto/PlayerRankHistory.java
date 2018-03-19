5e902e0e028a8b139316e9b0721f9a4226f2d99a
Elder Moraes <elder.moraes@gmail.com>
2018-03-18 20:31:47 -0700

    Microservices breakdown example

---
+++ 16930_microservices/microservice-gateway/src/main/java/com/eldermoraes/microservice/gateway/dto/PlayerRankHistory.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2018 eldermoraes.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.eldermoraes.microservice.gateway.dto;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author eldermoraes
+ */
+public class PlayerRankHistory implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    private Long id;
+    
+    private Long player;
+    
+    private Long rank;
+    
+    private Long points;
+    
+    private Date date;
+    
+    public PlayerRankHistory(){
+        
+    }
+
+    public PlayerRankHistory(Long id, Long player, Long rank, Long points, Date date) {
+        this.id = id;
+        this.player = player;
+        this.rank = rank;
+        this.points = points;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Long getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Long player) {
+        this.player = player;
+    }
+
+    public Long getRank() {
+        return rank;
+    }
+
+    public void setRank(Long rank) {
+        this.rank = rank;
+    }
+
+    public Long getPoints() {
+        return points;
+    }
+
+    public void setPoints(Long points) {
+        this.points = points;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }    
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof PlayerRankHistory)) {
+            return false;
+        }
+        PlayerRankHistory other = (PlayerRankHistory) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.eldermoraes.microservice.gateway.dto.PlayerRankHistory[ id=" + id + " ]";
+    }
+
+}
